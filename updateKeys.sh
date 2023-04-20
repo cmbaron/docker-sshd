8d021af8c817b2671e1fc1cc6040fdd6b078e8ed
Chris Baron <cmbaron@gmail.com>
2023-04-20 03:06:06 -0400

    fix image name

--- updateKeys.sh
+++ updateKeys.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Update the keys stored in ssh.yaml from the authorized-keys file
 kubectl --namespace default delete secret ssh-keys
-kubectl --namespace default create secret generic ssh-keys --from-file=~/ssh/.authorized_keys
+kubectl --namespace default create secret generic ssh-keys --from-file=~/.ssh/authorized_keys
